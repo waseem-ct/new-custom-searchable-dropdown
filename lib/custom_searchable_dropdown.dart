a24900b3cf2b089277344701c461116e17966a0c
Waseem Alam <146913828+waseem-ct@users.noreply.github.com>
2026-06-25 15:59:06 +0530

    Update custom_searchable_dropdown.dart

--- lib/custom_searchable_dropdown.dart
+++ lib/custom_searchable_dropdown.dart
@@ -45,7 +45,6 @@ class CustomSearchableDropDown extends StatefulWidget {
   final Widget? emptySearchWidget;
 
   const CustomSearchableDropDown({
-    super.key,
     required this.items,
     required this.label,
     required this.onChanged,
